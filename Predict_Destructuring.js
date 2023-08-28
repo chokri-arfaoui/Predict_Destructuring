2df0bdbc464c20bcccce78bbfe289f194baa7f85
arfaoui chokri <arfaouichokri601@gmail.com>
2023-08-29 00:14:59 +0100

    Predict Destructuring

---
+++ Predict_Destructuring.js
@@ -0,0 +1,64 @@
+// // problem 1
+// const cars = ['Tesla', 'Mercedes', 'Honda']
+// const [ randomCar ] = cars
+// const [ ,otherRandomCar ] = cars
+// //Predict the output
+// console.log(randomCar)
+// console.log(otherRandomCar)
+// // output : Tesla
+//          // Mercedes
+
+
+// // problem 2
+// const employee = {
+//     name: 'Elon',
+//     age: 47,
+//     company: 'Tesla'
+// }
+// const { name: otherName } = employee;
+// //Predict the output
+// console.log(name);
+// console.log(otherName);
+// // name not defined
+
+// // problem 3
+// const person = {
+//     name: 'Phil Smith',
+//     age: 47,
+//     height: '6 feet'
+// }
+// const password = '12345';
+// const { password: hashedPassword } = person;  
+// //Predict the output
+// // console.log(password);
+// console.log(hashedPassword);
+// output 12345
+// hashedPassword undefined
+
+
+// problem 4
+// const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+// const [,first] = numbers;
+// const [,,,second] = numbers;
+// const [,,,,,,,,third] = numbers;
+// //Predict the output
+// console.log(first == second);
+// console.log(first == third);
+// output False
+//         True
+
+
+
+
+
+// problem5
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+output 
+False 
+True
